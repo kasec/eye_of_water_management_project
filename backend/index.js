ef516c4f640074081a5b117200f02c03b5115e96
Gelaz el chingon <galfan18kasec@gmail.com>
2025-02-07 21:53:19 -0600

    Added supabase client and fetching real data

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const fs = require("fs");
 const app = express();
 const port = 3000;
+const { createClient } = require('@supabase/supabase-js')
+
+const supabaseUrl = 'https://sjgcphkymhrezykqnzze.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -10,17 +14,23 @@ app.use(function (req, res, next) {
 });
 
 // API REST
-app.get("/api/users", (req, res) => {
-    res.json({
-        clients: [
-            { name: "Azael" },
-            { name: "Joel" },
-            { name: "PEdro" },
-            { name: "Ramiro" },
-            { name: "Joaquin" },
-            { name: "JEsus" },
-        ],
-    });
+app.get("/api/users", async (req, res) => {
+    try {
+        console.log('Fetching users');
+
+        const { data, error } = await supabase
+            .from('users')
+            .select()
+
+        if (error) {
+            throw new Error(error);
+        }
+
+        res.json({ data });
+    } catch (err) {
+        console.error('An error fetching users was found', err);
+        res.status(500).json({ message: 'An error fetching users was found' });
+    }
 });
 app.get("/api/clients", (req, res) => {
     res.json({
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,22 +1,35 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
+
+type User = {
+  id: number;
+  created_at: string;
+  first_name: string;
+  last_name: string;
+};
 
 function App() {
-  const [users, setUsers] = useState([])
-  useEffect(()=> {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
     // if you don't understand below code, try to research how JS promises works
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-    fetch('http://localhost:3000/api/users', { method: 'get' })
-    .then(res => res.json())
-    .then((data) => setUsers(data.clients))
-  }, [])
+    fetch("http://localhost:3000/api/users", { method: "get" })
+      .then((res) => res.json())
+      .then((res) => setUsers(res?.data ?? []));
+  }, []);
+
+  console.log("Rendering Index.jsx", { users });
+
   return (
     <>
       <h1>Welcome to Ojo de agua management</h1>
       <ul>
-        {users.map(user => <li key={user.name}>{user.name}</li>)}
+        {users.map((user) => (
+          <li key={user.first_name}>{user.first_name}</li>
+        ))}
       </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
